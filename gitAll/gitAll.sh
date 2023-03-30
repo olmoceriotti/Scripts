59b8ceee19eb0d37f4872de591d88cdb2b3ff54f
Olmo Ceriotti <olmoceriotti@gmail.com>
2023-03-30 13:07:21 +0200

    First commit

---
+++ gitAll/gitAll.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+if [[ -z $1 ]]; then
+  echo -e "${RED}Failed: provide a commit message${NC}"
+else
+  git add .
+  git commit -m "\""$1"\""
+  git push origin main
+fi
